ae7d92b5dec61b3c7404f69cc417e1d4c7b084d0
C. Cifuentes <40443545+ccifuentesr@users.noreply.github.com>
2020-07-31 01:28:06 +0200

    Update cif20.plot_RA_DE.py

--- cif20.plot_RA_DE.py
+++ cif20.plot_RA_DE.py
@@ -7,6 +7,7 @@ import csv
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 import matplotlib.cm as cm
 from matplotlib import rc
 
